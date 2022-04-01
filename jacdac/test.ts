297cfb1603f23f91e0bb2284fee83138db454bfb
pelikhan <jhalleux@microsoft.com>
2022-04-01 04:43:17 -0700

    adding test

--- jacdac/test.ts
+++ jacdac/test.ts
@@ -1,1 +1,6 @@
-//
+forever(() => {
+    console.logValue("temp", modules.temperature1.temperature())
+    console.logValue("airp", modules.airPressure1.pressure())
+    console.logValue("humi", modules.humidity1.humidity())
+    pause(1000)
+})
