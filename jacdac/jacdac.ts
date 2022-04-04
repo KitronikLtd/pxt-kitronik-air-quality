527387580c1874be4aae4ce1bd84daab965494e5
pelikhan <jhalleux@microsoft.com>
2022-04-04 14:16:49 -0700

    fix: adding more support services

--- jacdac/jacdac.ts
+++ jacdac/jacdac.ts
@@ -1,5 +1,5 @@
 //% deprecated
-namespace kitronik_air_quality { }
+namespace kitronik_air_quality {}
 
 namespace servers {
     class CharacterScreenServer extends jacdac.Server {
@@ -45,51 +45,45 @@ namespace servers {
     }
 
     const YEAR_OFFSET = 2000
-    class RealTimeClockServer extends jacdac.Server {
+    class RealTimeClockServer extends jacdac.SensorServer {
         constructor() {
             super("clock", jacdac.SRV_REAL_TIME_CLOCK, {
                 variant: jacdac.RealTimeClockVariant.Crystal,
             })
         }
-        handlePacket(pkt: jacdac.JDPacket): void {
+
+        serializeState() {
+            const year = kitronik_air_quality.readDateParameter(
+                DateParameter.Year
+            )
+            const month = kitronik_air_quality.readDateParameter(
+                DateParameter.Month
+            )
+            const dayOfMonth = kitronik_air_quality.readDateParameter(
+                DateParameter.Day
+            )
+            const dayOfWeek = 0
+            const hour = kitronik_air_quality.readTimeParameter(
+                TimeParameter.Hours
+            )
+            const min = kitronik_air_quality.readTimeParameter(
+                TimeParameter.Minutes
+            )
+            const sec = kitronik_air_quality.readTimeParameter(
+                TimeParameter.Seconds
+            )
+            return jacdac.jdpack("u16 u8 u8 u8 u8 u8 u8", [
+                year + YEAR_OFFSET,
+                month,
+                dayOfMonth,
+                dayOfWeek,
+                hour,
+                min,
+                sec,
+            ])
+        }
+        handleCustomCommand(pkt: jacdac.JDPacket): void {
             if (
-                pkt.isRegGet &&
-                pkt.regCode == jacdac.RealTimeClockReg.LocalTime
-            ) {
-                const year = kitronik_air_quality.readDateParameter(
-                    DateParameter.Year
-                )
-                const month = kitronik_air_quality.readDateParameter(
-                    DateParameter.Month
-                )
-                const dayOfMonth = kitronik_air_quality.readDateParameter(
-                    DateParameter.Day
-                )
-                const dayOfWeek = 0
-                const hour = kitronik_air_quality.readTimeParameter(
-                    TimeParameter.Hours
-                )
-                const min = kitronik_air_quality.readTimeParameter(
-                    TimeParameter.Minutes
-                )
-                const sec = kitronik_air_quality.readTimeParameter(
-                    TimeParameter.Seconds
-                )
-                this.handleRegFormat(
-                    pkt,
-                    jacdac.RealTimeClockReg.LocalTime,
-                    "u16 u8 u8 u8 u8 u8 u8",
-                    [
-                        year + YEAR_OFFSET,
-                        month,
-                        dayOfMonth,
-                        dayOfWeek,
-                        hour,
-                        min,
-                        sec,
-                    ]
-                )
-            } else if (
                 pkt.isCommand &&
                 pkt.serviceCommand == jacdac.RealTimeClockCmd.SetTime
             ) {
@@ -104,7 +98,7 @@ namespace servers {
                 )
                 kitronik_air_quality.setTime(hour, min, sec)
                 console.log(`${dayOfMonth}, ${month}, ${year}`)
-            }
+            } else pkt.possiblyNotImplemented()
         }
     }
 
@@ -112,17 +106,26 @@ namespace servers {
         if (!jacdac.isSimulator()) {
             kitronik_air_quality.bme688Init()
             kitronik_air_quality.setupGasSensor()
-            kitronik_air_quality.calcBaselines()
+            const STREAMING_INTERVAL = 1000
+            forever(() => {
+                kitronik_air_quality.measureData()
+                pause(STREAMING_INTERVAL)
+            })
             // start all servers on hardware
             const servers: jacdac.Server[] = [
                 jacdac.createSimpleSensorServer(
                     "temperature",
                     jacdac.SRV_TEMPERATURE,
                     "i22.10",
-                    () =>
-                        kitronik_air_quality.readTemperature(
+                    () => {
+                        led.toggle(0, 0)
+                        return kitronik_air_quality.readTemperature(
                             kitronik_air_quality.TemperatureUnitList.C
                         )
+                    },
+                    {
+                        streamingInterval: STREAMING_INTERVAL,
+                    }
                 ),
                 jacdac.createSimpleSensorServer(
                     "pressure",
@@ -131,22 +134,30 @@ namespace servers {
                     () =>
                         kitronik_air_quality.readPressure(
                             kitronik_air_quality.PressureUnitList.Pa
-                        ) / 100
+                        ) / 100,
+                    {
+                        streamingInterval: STREAMING_INTERVAL,
+                    }
                 ),
                 jacdac.createSimpleSensorServer(
                     "humidity",
                     jacdac.SRV_HUMIDITY,
                     "u22.10",
-                    () => kitronik_air_quality.readHumidity()
+                    () => kitronik_air_quality.readHumidity(),
+                    {
+                        streamingInterval: STREAMING_INTERVAL,
+                    }
                 ),
-                /* does not work
                 jacdac.createSimpleSensorServer(
                     "eCO2",
                     jacdac.SRV_E_CO2,
                     "u22.10",
-                    () => readeCO2()
+                    () => kitronik_air_quality.readeCO2(),
+                    {
+                        streamingInterval: STREAMING_INTERVAL,
+                        calibrate: () => kitronik_air_quality.calcBaselines(),
+                    }
                 ),
-                */
                 new RealTimeClockServer(),
                 new CharacterScreenServer(),
             ]
@@ -165,29 +176,45 @@ namespace modules {
      * The air temperature sensor on the Kitronik air quality module
      */
     //% fixedInstance whenUsed block="kitronik temperature"
-    export const kitronikTemperature = new TemperatureClient("kitronik temperature?device=self")
+    export const kitronikTemperature = new TemperatureClient(
+        "kitronik temperature?device=self"
+    )
 
     /**
      * The air pressure sensor on the Kitronik air quality module
      */
     //% fixedInstance whenUsed block="kitronik pressure"
-    export const kitronikPressure = new AirPressureClient("kitronik pressure?device=self")
+    export const kitronikPressure = new AirPressureClient(
+        "kitronik pressure?device=self"
+    )
 
     /**
      * The air humidity sensor on the Kitronik air quality module
      */
     //% fixedInstance whenUsed block="kitronik air humidity"
-    export const kitronikHumidity = new HumidityClient("kitronik humidity?device=self")
+    export const kitronikHumidity = new HumidityClient(
+        "kitronik humidity?device=self"
+    )
+
+    /**
+     * The CO2 sensor on the Kitronik air quality module
+     */
+    //% fixedInstance whenUsed block="kitronik CO2"
+    export const kitronikCO2 = new ECO2Client("kitronik CO2?device=self")
 
     /**
      * The character screen display on the Kitronik air quality module
      */
     //% fixedInstance whenUsed block="kitronik air CO2"
-    export const kitronikDisplay = new CharacterScreenClient("kitronik display?device=self")
+    export const kitronikDisplay = new CharacterScreenClient(
+        "kitronik display?device=self"
+    )
 
     /**
      * The real time clock client on the Kitronik air quality module
      */
     //% fixedInstance whenUsed block="kitronik clock"
-    export const kitronikClock = new RealTimeClockClient("kitronik clock?device=self")
+    export const kitronikClock = new RealTimeClockClient(
+        "kitronik clock?device=self"
+    )
 }
--- jacdac/test.ts
+++ jacdac/test.ts
@@ -2,10 +2,23 @@ forever(() => {
     console.logValue("temp", modules.kitronikTemperature.temperature())
     console.logValue("pres", modules.kitronikPressure.pressure())
     console.logValue("humi", modules.kitronikHumidity.humidity())
+    console.logValue("eco2", modules.kitronikCO2.eCO2())
 
-    modules.kitronikDisplay.setLine(0, "temp:" + modules.kitronikTemperature.temperature())
-    modules.kitronikDisplay.setLine(1, "pres:" + modules.kitronikPressure.pressure())
-    modules.kitronikDisplay.setLine(2, "humi:" + modules.kitronikHumidity.humidity())
+    modules.kitronikDisplay.setLine(0, "milli:" + control.millis())
+    modules.kitronikDisplay.setLine(1, "clock:" + modules.kitronikClock.hour() + ":" + modules.kitronikClock.min() + ":" + modules.kitronikClock.sec())
+    modules.kitronikDisplay.setLine(
+        2,
+        "temp:" + modules.kitronikTemperature.temperature()
+    )
+    modules.kitronikDisplay.setLine(
+        3,
+        "pres:" + modules.kitronikPressure.pressure()
+    )
+    modules.kitronikDisplay.setLine(
+        4,
+        "humi:" + modules.kitronikHumidity.humidity()
+    )
+    modules.kitronikDisplay.setLine(5, "eco2:" + modules.kitronikCO2.eCO2())
 
     pause(1000)
 })
